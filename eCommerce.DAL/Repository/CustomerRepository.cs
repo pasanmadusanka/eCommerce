2720cca5ac5d9d0d1512a63402dc839c2a56a758
pasanmadusanka <pasanmadusanka29@gmail.com>
2016-03-25 12:14:56 +0530

    Initial with create basics
    Create Library projects and add one MVC web site and combine them.

---
+++ eCommerce.DAL/Repository/CustomerRepository.cs
@@ -0,0 +1,20 @@
+﻿using eCommerce.DAL.Data;
+using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Repository
+{
+    public class CustomerRepository : RepositoryBase<Customer>
+    {
+        public CustomerRepository(DataContext context)
+            : base(context)
+        {
+            if(context == null)
+            { throw new ArgumentNullException(); }
+        }
+    }
+}
---
+++ eCommerce.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using eCommerce.Contracts.Repository;
+using eCommerce.DAL.Data;
+using eCommerce.DAL.Repository;
+using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRepositoryBase<Customer> customers;
+        IRepositoryBase<Product> products;
+        public HomeController(IRepositoryBase<Customer> customers, IRepositoryBase<Product> products)
+        {
+            this.customers = customers;
+            this.products = products;
+        }
+        public ActionResult Index()
+        {
+            var productList = products.GetAll();
+            return View(productList);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var productDet = products.GetById(id);
+            return View(productDet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
